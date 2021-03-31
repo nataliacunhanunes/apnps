57a69eb5915bc22179fbf48617266a200028e422
Natalia Cunha <nataliacunha.com>
2021-03-31 10:37:50 -0300

    mudança index.js

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const express = require ('express')
 const app = express()
 
-app.get('/', function(rep,res){
+app.get('/', function(req,res){
 
     res.send('Ola Naty.')
 })
 
-app.get('/msg', function(rep,res){
+app.get('/msg', function(req,res){
     res.send("Essa mensagem é automática!")})
 
-app.get('/sobre', function(rep,res){
+app.get('/sobre', function(req,res){
         res.send("Essa página está sendo devolvida pela Naty")})
+
+app.get('/layout', function(req,res){
+            res.send("Esse é o novo layout")})
 
 app.listen(3000,function(){
 
